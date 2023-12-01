b36ac4aa4e8fcea3135bc190cefbb07a5fde9f6b
Kaspars Lasinskis <kasparsll@gmail.comm>
2023-12-01 17:21:42 +0200

    initial commit of v2

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,16 @@ import { Route, Routes } from 'react-router-dom';
 import Header from './components/Header/Header';
 import HomePage from './pages/HomePage';
 import ProductDetailsPage from './pages/ProductDetailsPage';
+import LogoIcon from './components/LogoIcon/LogoIcon';
 
 function App() {
   return (
     <>
       <Header />
+      <LogoIcon />
       <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="device/:deviceid" element={<ProductDetailsPage />} />
+        <Route path='/' element={<HomePage />} />
+        <Route path='device/:deviceid' element={<ProductDetailsPage />} />
       </Routes>
     </>
   );
