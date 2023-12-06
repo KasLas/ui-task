9a99d52f0883fc29ad61510223a8a6b3084618cc
Kaspars Lasinskis <kasparsll@gmail.comm>
2023-12-06 14:35:48 +0200

    removed unused files

--- src/main.tsx
+++ src/main.tsx
@@ -4,7 +4,6 @@ import { BrowserRouter } from 'react-router-dom';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ChakraProvider } from '@chakra-ui/react';
 import App from './App.tsx';
-import './index.css';
 import { extendedTheme } from './theme/extendedTheme.ts';
 
 const queryClient = new QueryClient();
--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,11 +1,6 @@
 export const DEVICES_URL = 'https://static.ui.com/fingerprint/ui/public.json';
 export const IMG_BASE_URL = 'https://static.ui.com/fingerprint/ui/icons/';
 
-export enum COLORS {
-  defaultBorder = '#ededf0',
-  darkBorder = '#DBDCE1',
-}
-
 export enum IMG_DIMENSIONS {
   sm = 25,
   md = 101,
